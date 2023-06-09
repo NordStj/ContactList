c3c8d0c3778ca2d45cc26e76f59fb978e699ab4e
NordStj <Neoskyping@gmail.com>
2023-06-09 10:16:31 +0300

    final app

--- ContactList/SecondContactListController.swift
+++ ContactList/SecondContactListController.swift
@@ -26,10 +26,19 @@ class SecondContactListController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         var content = cell.defaultContentConfiguration()
-        let contact = person[indexPath.row]
-        content.text = contact.fullName
+        let contact = person[indexPath.section]
+        switch indexPath.row {
+        case 0:
+            content.text = "Phone: \(contact.phone)"
+        default:
+            content.secondaryText = "Email: \(contact.email)"
+        }
         cell.contentConfiguration = content
         return cell
     }
 
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        let person = person[section]
+        return person.fullName
+    }
 }
